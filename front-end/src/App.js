97d2c35e7d615878e32e205a0360b34ff256b176
xinrantaoangela <xinran.tao2001@gmail.com>
2023-10-15 19:13:15 -0400

    Angela's Sprint 1, add the home page, login page, and signup page front end code. Add front end code for footer and header.

---
+++ front-end/src/App.js
@@ -0,0 +1,30 @@
+// app.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomeScreen from './HomeScreen';
+import EditInformation from './EditInformation'
+import SavedConnections from './SavedConnections'
+import GenerateCode from './GenerateCode'
+import Header from './Header'
+import Footer from './Footer'
+import Login from './Login'
+import Signup from './Signup'
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<HomeScreen />} />
+        <Route path="/edit-information" element={<EditInformation />} />
+        <Route path="/saved-connections" element={<SavedConnections />} />
+        <Route path="/generate-code" element={<GenerateCode />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ front-end/src/HomeScreen.js
@@ -0,0 +1,21 @@
+// HomeScreen.js
+import React from 'react';
+import './HomeScreen.css';
+
+const HomeScreen = () => {
+    return (
+      <div className="home-container">
+        <div className="home-header">
+          <img src="path_to_logo" alt="App Logo" className="app-logo" />
+          <p>App Description: Lorem ipsum dolor sit...</p>
+        </div>
+        <div className="home-links">
+          <a href="/edit-information" className="home-link">Edit Information</a>
+          <a href="/saved-connections" className="home-link">Saved Connections</a>
+          <a href="/generate-code" className="home-link">Generate Code</a>
+        </div>
+      </div>
+    );
+}
+
+export default HomeScreen;
