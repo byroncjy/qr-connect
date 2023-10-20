99afbbcd0f2e158a41f59a47a44f22536d1ed1da
Sivan Cooperman <sivancooperman@gmail.com>
2023-10-19 22:00:26 -0400

    Add linter config, select all button, api data
    Adds packages required for linting, routing, and GETting data from an api. Mock data is now retrieved by the Select Info page to be displayed in a checkbox list. A new Select All button is added. Checked-ness of boxes is now properly tracked. Currently there is some backup data in SelectForm.js for when Mockaroo has cut me off for the day, but should be removed later. Still need CSS.

--- front-end/src/Select.js
+++ front-end/src/Select.js
@@ -1,15 +1,15 @@
-import React from "react"
-import { Link } from "react-router-dom"
-import SelectForm from "./SelectForm"
-import "./Select.css"
+import React from 'react'
+import { Link } from 'react-router-dom'
+import SelectForm from './SelectForm'
+import './Select.css'
 
 const Select = props => {
-	return (
-		<div className="SelectForm">
-			<Link to="/">Back</Link>
-			<SelectForm />
-		</div>
-	)
+    return (
+        <div className="SelectForm">
+            <Link to="/">Back</Link>
+            <SelectForm />
+        </div>
+    )
 }
 
 export default Select
