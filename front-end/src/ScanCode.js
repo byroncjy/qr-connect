bbe42f3bd695a86c6011304a13b8dde3b3e2395f
Pan-LLi <pl1989@nyu.edu>
2023-10-23 23:09:24 -0400

    Axios and Fetch added

--- front-end/src/ScanCode.js
+++ front-end/src/ScanCode.js
@@ -1,13 +1,36 @@
-import { Link } from 'react-router-dom'
-import './ScanCode.css'
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './ScanCode.css';
 
-function ScanCode(){
-          return(     
-          <div className='Box'>
-          <div><Link to = "/">Home</Link></div>
-             <img className = "Logo" src="../instagram-circle-icon.png"></img>
-             <div className = "ScanButton">Scan</div>  
-          </div>       
+
+function ScanCode() {
+          const [imageUrl, setImageUrl] = useState('');
+
+          useEffect(() => {
+                    async function fetchImage() {
+                              try {
+                                        const LogoUrl = 'https://picsum.photos/200/300'
+                                        await axios.get(LogoUrl)
+                                                  .then(response => {
+                                                            if (response.status === 200) {
+                                                                      setImageUrl(LogoUrl);
+                                                            }
+                                                  })
+                              } catch (error) {
+                                        console.error(error);
+                              }
+                    }
+
+                    fetchImage();
+          }, []);
+
+          return (
+                    <div className='Box'>
+                              <div><Link to="/">Home</Link></div>
+                              <img className="Logo" src={imageUrl}></img>
+                              <div className="ScanButton">Scan</div>
+                    </div>
           );
 }
 
