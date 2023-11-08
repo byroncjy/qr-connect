caeea90e4163ca12d766752244b23a9a0b6edc56
SivanC <sc7443@nyu.edu>
2023-11-08 17:51:44 -0500

    Add more account info tests
    Add tests for PUTting and GETting account information from the user id routes.

--- back-end/test/test-user.js
+++ back-end/test/test-user.js
@@ -14,11 +14,11 @@ describe('User Routes', function () {
         const id = 0
         chai
           .request(router)
-          .get('/user/' + id)
+          .get(`/user/${id}`)
           .end((err, res) => {
             expect(err).to.be.null
             expect(res).to.have.status(200)
-            espect(res.body).to.be.a.('object')
+            expect(res.body).to.be.a.('object')
             expect(res.body).to.have.all.keys('email', 'first_name', 'last_name', 'url_picture')
             done()
           })
@@ -26,12 +26,51 @@ describe('User Routes', function () {
     }),
     describe('Unsuccessful GET', function () {
       it('should respond with an HTTP 500 status code and an error', function () {
-        
+        chai
+          .request(router)
+          .get('/user/notanid')
+          .end(err, res) => {
+            expect(res).to.have.status(500)
+            expect(res.body).to.be.a.('object')
+            expect(res).to.have.keys('error')
+            done()
       })
     }),
     describe('Successful PUT', function () {
+      it('should respond with an HTTP 200 status and confirmation message', function () {
+        chai
+          .request(router)
+          .put(`/user/${id}`)
+          .send({
+            email: 'email',
+            first_name: 'first_name',
+            last_name: 'last_name',
+            url_picture: 'url_picture'
+          })
+          .end((err, res) => {
+            expect(err).to.be.null
+            expect(res).to.have.status(200)
+            expect(res.body).to.be.a.('object')
+            expect(res.body).to.have.keys('message')
+            done()
+          })
+      })
     }),
     describe('Unsuccessful PUT', function() {
+      it('should respond with an HTTP 500 status and error message'), function () {
+        chai
+          .request(router)
+          .put(`/user/${id}`)
+          .send({
+            bad_data: 'bad_data'
+          })
+          .end((err, res) => {
+            expect(res).to.have.status(500)
+            expect(res.body).to.be.a.('object')
+            expect(res).to.have.keys('error')
+            done()
+          })
+      })
     })
   }),
   // personal information added by the user
