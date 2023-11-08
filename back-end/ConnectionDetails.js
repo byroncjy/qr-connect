f0f23ada7d76febacc1dfeb3e2e8148cb426e6eb
Pan-LLi <pl1989@nyu.edu>
2023-11-08 12:59:06 -0500

    Backend added

--- back-end/ConnectionDetails.js
+++ back-end/ConnectionDetails.js
@@ -34,7 +34,7 @@ app.post('/ScanCode', async (req, res) => {
     const code = jsQR(imageData.data, imageData.width, imageData.height)
 
     if (code) {
-      res.json({ qrCodeText: code.data })
+      res.json({ qrCodeText: code.data, redirect: '/ConnectionDetails' })
     } else {
       res.status(400).send('No QR code found.')
     }
@@ -43,6 +43,13 @@ app.post('/ScanCode', async (req, res) => {
     res.status(500)
   }
 })
+
+/*
+app.get('/ConnectionDetails', (req, res) => {
+  const scanResultUrl = 'https://my.api.mockaroo.com/QRcodeResult.json?key=723ed310'
+  res.json({ scanResultUrl })
+})
+*/
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
