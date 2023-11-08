8e0be6880dba0b32d2c5a8517a4fe07f5b3f9d10
larahynkim <lara.kimcf@gmail.com>
2023-11-09 06:54:58 +0900

    revise backend for SavedConnections

--- front-end/src/ConnectionDetails.js
+++ front-end/src/ConnectionDetails.js
@@ -1,69 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import PropTypes from 'prop-types';
+import './ConnectionDetails.css';
 
-import { Link } from "react-router-dom";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./ConnectionDetails.css";
+const ConnectionDetails = ({ ScannedInfo }) => {
+  const [imageUrl, setImageUrl] = useState('');
 
-const ConnectionDetails = () => {
+  useEffect(() => {
+    async function fetchImage() {
+      try {
+        const logoUrl = 'https://picsum.photos/200/300';
+        const response = await axios.get(logoUrl);
+        if (response.status === 200) {
+          setImageUrl(logoUrl);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-	const [scanResult, setScanResult] = useState([]);
+    fetchImage();
+  }, []);
 
-	useEffect(() => {
-		async function fetchScanResult() {
-			try {
-				const scanResultUrl = "https://my.api.mockaroo.com/QRcodeResult.json?key=723ed310";
-				await axios.get(scanResultUrl)
-					.then(response => {
-						if (response.status === 200) {
-							setScanResult(response.data);
-							console.log(response.data);
+  return (
+    <div className='Box'>
+      <div className="DetailsContainer">
+        <div className="areaA">
+          <img className="PlatForms" src={imageUrl} alt="Platform Icon" />
+        </div>
+        <div className="areaB">{ScannedInfo}Bot 1</div>
+        <div className="areaC">
+          <div className="ViewCode">
+            <Link to="/ViewCode">ViewCode</Link>
+          </div>
+        </div>
+        <div className="areaD">
+          <ul>
+            <li>instagram introduction 1</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s.</li>
+            <li>instagram introduction 1</li>
+          </ul>
+        </div>
+        <div className="areaE">
+          <ul>
+            <li>instagram introduction 2</li>
+            <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s.</li>
+            <li>instagram introduction 2</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-						}
-					});
-			} catch (error) {
-				console.error(error);
-			}
-		}
-
-
-		fetchScanResult();
-
-	}, []);
-
-	return (
-		
-		<div className='Box'>	
-			{scanResult.map((item, index) => (
-				<div className="detailsContainer" key={index}>
-					<div className="areaA">
-						<img className="PlatForms" src={item.webImage} alt="" />
-					</div>
-					<div className="areaB" style={{ backgroundColor: item.mediaColor }}>
-						{item.webName}
-					</div>
-					<div className="areaD">
-						<ul>
-							<li>{item.Description}</li>
-							<li>{item.Description}</li>
-						</ul>
-					</div>
-					<div className="areaE">
-						<ul>
-							<li>{item.Description}</li>
-							<li>{item.Description}</li>
-						</ul>
-					</div>
-				</div>
-			))}
-
-			<div className="">
-					<div className="viewCode">
-							<Link to="/ViewCode"> View Code </Link>
-						</div>
-					</div>
-		</div>
-
-	);
+ConnectionDetails.propTypes = {
+  ScannedInfo: PropTypes.string.isRequired
 };
 
 export default ConnectionDetails;
