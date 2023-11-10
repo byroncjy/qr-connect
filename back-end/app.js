0c3cb10e04ff382a0752a631e3a44d9995b8d801
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-09 22:49:28 -0500

    change .env file and edit the backend for dotenv configuration

--- back-end/app.js
+++ back-end/app.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 const unchangedImagesRouter = require('./unchanged-images'); // Ensure this path is correct
 const app = express();
 
+require('dotenv').config({ silent: true }) // load environmental variables from a hidden file named .env
+
 app.use(cors());
 // Use the unchanged images router for requests to '/images'
 app.use('/images', unchangedImagesRouter);
