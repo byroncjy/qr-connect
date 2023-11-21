858fcd5d32e6f8349b98bcc66f71ccb153ea9cb2
Pan-LLi <pl1989@nyu.edu>
2023-11-20 21:03:13 -0500

    User Profile Schema Created

---
+++ back-end/models/userProfileSchema.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+const UserProfileSchema = new mongoose.Schema({
+  platform: {
+    type: String,
+    required: true
+  },
+  details: {
+    type: String,
+    required: true
+  },
+  username: {
+    type: String
+  }
+});
+
+const UserProfile = mongoose.model('UserProfile', UserProfileSchema);
+
+module.exports = UserProfile;
