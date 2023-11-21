3bd8086848009f18dd66022449b9e95e4933b5db
Pan-LLi <pl1989@nyu.edu>
2023-11-20 21:18:33 -0500

    Schema for Multiple platforms added

--- back-end/models/userProfileSchema.js
+++ back-end/models/userProfileSchema.js
@@ -1,26 +1,46 @@
 const mongoose = require('mongoose');
 
+const PlatformSchema = new mongoose.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    }
+});
+
+const ConnectionSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    platforms: [PlatformSchema],
+    connectedAt: {
+        type: Date,
+        default: Date.now
+    }
+
+});
 
 const UserProfileSchema = new mongoose.Schema({
-  platform: {
-    type: String,
-    required: true
-  },
-  details: {
-    type: String,
-    required: true
-  },
-  first_name: {
-    type: String,
-    required: true
-  },
-  last_name: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String
-  }
+    userId: {
+        type: Number,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String
+    },
+    connections: [ConnectionSchema]
 });
 
 const UserProfile = mongoose.model('UserProfile', UserProfileSchema);
