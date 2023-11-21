3bdfd3f6688dbc0400f5369c7b133537da0832a1
Pan-LLi <pl1989@nyu.edu>
2023-11-20 22:07:15 -0500

    accountSchema edited

--- back-end/models/userProfileSchema.js
+++ back-end/models/userProfileSchema.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose');
 const AccountSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
     unique: true
   },
   password_hash: {
-    type: String,
-    required: true
+    type: String
   }
 });
 
