db2b68a64fd375216c8e41d1fe3a100d2043fdde
SivanC <sc7443@nyu.edu>
2023-11-24 17:06:23 -0500

    Update Select Information data logic and file names
    Rename SelectInformationForm to SelectInformation because SelectInformation doesn't exist anymore, and this is more clear. Changes the select information page to no longer care about platform id/depend on index of array being the same as id. Updates old references to data attributes to reflect new data schema.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,7 @@ import ConnectionDetails from "./ConnectionDetails";
 import PrivacyPolicy from "./PrivacyPolicy";
 import ContactUs from "./ContactUs";
 import TermOfServices from "./TermOfServices";
-import SelectInformationForm from "./SelectInformationForm"
+import SelectInformation from "./SelectInformation"
 import SavedConnections from "./SavedConnections"
 import EditInformation from "./EditInformation"
 import './App.css';
@@ -32,7 +32,7 @@ const App = () => {
 				<Route path="/ContactUs" element={<ContactUs />} />
 				<Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
 				<Route path="/scan-code" element={<ScanCode />} />
-				<Route path="/select-information" element={<SelectInformationForm />} />
+				<Route path="/select-information" element={<SelectInformation/>} />
 				<Route path="/saved-connections" element={<SavedConnections />} />
 				<Route path="/edit-information" element={<EditInformation />} />
 			</Routes>
