8bb441207c40bff4fdd26d54321b2a9d6f325f71
SivanC <sc7443@nyu.edu>
2023-11-25 14:03:48 -0500

    Remove account schema from User schema

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -43,7 +43,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  account: accountSchema,
   platforms: [platformSchema],
   connections: [connectionSchema]
 });
