7fa23b7778f0ad48b2bc202c02052af0f6432eba
larahynkim <lara.kimcf@gmail.com>
2023-11-28 11:37:11 +0900

    revise with suggestions

--- back-end/connections.js
+++ back-end/connections.js
@@ -9,11 +9,12 @@ const apiBaseUrl = process.env.API_BASE_URL;
 const apiKey = process.env.LARA_API_KEY;
 const authenticateToken = require('./authRoutes');
 
-router.get('/saved-connections', authenticateToken, async (req, res, next) => {
+// Route for saved connections
+router.get('/connections', authenticateToken, async (req, res, next) => {
   if (process.env.SIMULATE_ERROR === 'true') {
     const err = new Error('Simulated server error');
     err.status = 500;
-    return next(err); 
+    return next(err);
   }
 
   try {
@@ -28,15 +29,17 @@ router.get('/saved-connections', authenticateToken, async (req, res, next) => {
   }
 });
 
-router.get('/user-connections/:userId', async (req, res) => {
+// Route for user-specific connections
+router.get('/connections/:userId', async (req, res) => {
   try {
     const userId = req.params.userId;
-    console.log('User ID:', userId); 
+    console.log('User ID:', userId);
 
-    const userConnections = await User.findById(userId).select('connections').populate('connections');
-    console.log('User Connections:', userConnections);     if (!userConnections) {
+    const userConnections = await User.findById(userId).select('connections');
+    if (!userConnections) {
       return res.status(404).json({ error: 'User not found' });
     }
+    console.log('User Connections:', userConnections);
     res.json(userConnections.connections);
   } catch (error) {
     console.error('Error fetching user connections:', error);
@@ -44,6 +47,7 @@ router.get('/user-connections/:userId', async (req, res) => {
   }
 });
 
+// Error handling middleware
 router.use((err, req, res, next) => {
   console.error(err);
   res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
