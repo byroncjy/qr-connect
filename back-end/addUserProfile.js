ef97de0f51f79904632b74a9e7f97698820d6fdc
Pan-LLi <pl1989@nyu.edu>
2023-11-28 01:39:21 -0500

    find user added

--- back-end/addUserProfile.js
+++ back-end/addUserProfile.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const { Connection } = require('./models/User');
+const { Connection, User } = require('./models/User');
+
+router.get('/user/:userId/platforms', async (req, res) => {
+          try {
+              const userId = req.params.userId;
+      
+
+              const user = await User.findById(userId).populate('connections.platforms');
+
+              if (!user) {
+                  return res.status(404).send('User not found');
+              }
+      
+
+              const platforms = user.connections.map(connection => connection.platforms).flat();
+      
+              res.status(200).json(platforms);
+          } catch (error) {
+              res.status(500).json({ message: error.message });
+          }
+      });
 
 router.post('/saveConnection', async (req, res) => {
           try {
