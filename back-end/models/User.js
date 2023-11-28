1815c08922b89d81c3f2aecb3dfd74ca9cd0bed0
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-28 00:40:05 -0500

    modified:   back-end/models/User.js

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId; 
-const platformSchema = new mongoose.Schema({
+const ObjectId = mongoose.Schema.Types.ObjectId; const platformSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
