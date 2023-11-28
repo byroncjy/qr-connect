4cc68606496d19508860b55ab9d80712365400fc
byroncjy <byroncjy@gmail.com>
2023-11-28 12:34:54 -0500

    Change

--- back-end/app.js
+++ back-end/app.js
@@ -44,6 +44,7 @@ try {
 // Routes
 app.use('/images', unchangedImagesRouter);
 app.use('/api/auth', authRoutes);
+// These are both set to users to make backend route more intuitive
 app.use('/users', userRoutes);
 app.use('/users', platformRoutes);
 app.use('/api', connectionsRoutes);
