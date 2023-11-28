1dfaa495070d2dd9c31af025b5f4f920eed25502
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-28 00:58:59 -0500

    conflicts

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const ObjectId = mongoose.Schema.Types.ObjectId; const platformSchema = new mongoose.Schema({
+const { ObjectId } = mongoose.Schema.Types;
   name: {
     type: String,
     required: true
