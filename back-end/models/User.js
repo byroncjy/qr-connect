b46629ca41f2900c4501d6aa50efb04daab681f5
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-28 00:59:50 -0500

    conflcits resolve

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { ObjectId } = mongoose.Schema.Types;
+const platformSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true
@@ -9,6 +10,7 @@ const { ObjectId } = mongoose.Schema.Types;
     required: true
   }
 });
+
 
 const connectionSchema = new mongoose.Schema({
   friend_id: {
