b5ce54c8dbaf4ec25a9fbcb1e49c9a669fc320a5
Pan-LLi <pl1989@nyu.edu>
2023-11-29 14:12:10 -0500

    typo fixed

--- back-end/ConnectionDetails.js
+++ back-end/ConnectionDetails.js
@@ -26,59 +26,58 @@ router.post('/ScanCode', async (req, res) => {
   const { qrData } = req.body;
 
   if (!qrData) {
-    console.log('No QR data received in request.');
-    return res.status(400).send('No QR data provided.');
+    console.log('No QR data received in request.')
+    return res.status(400).send('No QR data provided.')
   }
 
-  const base64Data = qrData.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+  const base64Data = qrData.replace(/^data:image\/(png|jpeg|jpg);base64,/, '')
 
   try {
-    console.log('Loading image from QR data...');
-    const image = await loadImage(`data:image/png;base64,${base64Data}`);
-    const canvas = createCanvas(image.width, image.height);
-    const context = canvas.getContext('2d');
+    console.log('Loading image from QR data...')
+    const image = await loadImage(`data:image/png;base64,${base64Data}`)
+    const canvas = createCanvas(image.width, image.height)
+    const context = canvas.getContext('2d')
 
-    console.log('Drawing image on canvas...');
-    context.drawImage(image, 0, 0);
-    const imageData = context.getImageData(0, 0, image.width, image.height);
+    context.drawImage(image, 0, 0)
+    const imageData = context.getImageData(0, 0, image.width, image.height)
 
-    console.log('Decoding QR code...');
-    const code = jsQR(imageData.data, imageData.width, imageData.height);
+    console.log('Decoding QR code...')
+    const code = jsQR(imageData.data, imageData.width, imageData.height)
 
     if (code) {
-      console.log(`QR Code found: ${code.data}`);
-      res.json({ qrCodeText: code.data, qrImageBase64: base64Data });
+      console.log(`QR Code found: ${code.data}`)
+      res.json({ qrCodeText: code.data, qrImageBase64: base64Data })
     } else {
-      console.log('No QR code found in the provided image.');
-      res.status(400).send('No QR code found.');
+      console.log('No QR code found in the provided image.')
+      res.status(400).send('No QR code found.')
     }
   } catch (error) {
-    console.error('Error processing QR code:', error);
-    res.status(500).send('Error processing QR code');
+    console.error('Error processing QR code:', error)
+    res.status(500).send('Error processing QR code')
   }
 });
 
 router.post('/ConnectionDetails', async (req, res) => {
   const { qrCodeText } = req.body;
-  console.log(`QR Code Text: ${qrCodeText}`);
+  console.log(`QR Code Text: ${qrCodeText}`)
 
   if (!isValidObjectId(qrCodeText)) {
-    return res.status(400).send('Invalid User ID');
+    return res.status(400).send('Invalid User ID')
   }
 
   try {
    
-    const user = await User.findOne({ _id: qrCodeText });
-    console.log('User found:', user);
+    const user = await User.findOne({ _id: qrCodeText })
+    console.log('User found:', user)
 
     if (!user) {
-      return res.status(404).send('User not found');
+      return res.status(404).send('User not found')
     }
 
     res.status(200).json(user);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ message: 'Internal Server Error' });
+    console.error('Error:', error)
+    res.status(500).json({ message: 'Internal Server Error' })
   }
 });
 
