4bffa5009b10c87697907b8f13bd0d48e20c1c56
xinrantaoangela <xinran.tao2001@gmail.com>
2023-11-30 14:08:40 -0500

    edit footer to make it static when  enough content and fixed when not enough content

--- front-end/src/Footer.js
+++ front-end/src/Footer.js
@@ -1,10 +1,31 @@
-// Footer.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Footer.css";
 
 const Footer = () => {
+    const [isFixed, setIsFixed] = useState(false);
+
+    useEffect(() => {
+        const checkHeight = () => {
+            const footerHeight = document.querySelector('.footer-container').offsetHeight;
+            const headerHeight = document.querySelector('.header-container') ? document.querySelector('.header-container').offsetHeight : 0; // Replace with your header selector if you have one
+            const contentHeight = window.innerHeight - headerHeight - footerHeight;
+
+            if (document.body.scrollHeight > contentHeight) {
+                setIsFixed(false);
+            } else {
+                setIsFixed(true);
+            }
+        };
+
+        checkHeight();
+
+        window.addEventListener('resize', checkHeight);
+
+        return () => window.removeEventListener('resize', checkHeight);
+    }, []);
+
     return (
-        <div className="footer-container">
+        <div className={isFixed ? "footer-container footer-fixed" : "footer-container"}>
             <p className="footer-text">© 2023 My App. All Rights Reserved.</p>
             <div className="footer-links">
                 <a href='/PrivacyPolicy'>Privacy Policy</a>
