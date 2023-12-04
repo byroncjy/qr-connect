a836a0d769cef02cf7b25fe4a584eed391fd0b9d
Pan-LLi <pl1989@nyu.edu>
2023-12-04 11:26:14 -0500

    remove comma

--- back-end/generateQRCode.js
+++ back-end/generateQRCode.js
@@ -1,27 +1,27 @@
-const { body, validationResult } = require('express-validator');
-const express = require('express');
-const router = express.Router();
-require('dotenv').config();
-const User = require('./models/User.js');
+const { body, validationResult } = require('express-validator')
+const express = require('express')
+const router = express.Router()
+require('dotenv').config()
+const User = require('./models/User.js')
 
 router.post('/GenerateQRCode', 
     body('decodedToken').notEmpty().withMessage('Decoded token is required'),
     body('decodedToken.userId').notEmpty().withMessage('User ID is required'),
 
     async (req, res) => {
-        const errors = validationResult(req);
+        const errors = validationResult(req)
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({ errors: errors.array() })
         }
 
-        const { decodedToken } = req.body;
+        const { decodedToken } = req.body
 
         try {
-            res.json({ decodedToken });
+            res.json({ decodedToken })
         } catch (error) {
-            res.status(500).json({ error: 'Server error' });
+            res.status(500).json({ error: 'Server error' })
         }
     }
-);
+)
 
-module.exports = router;
+module.exports = router
--- front-end/src/GenerateQRCode.js
+++ front-end/src/GenerateQRCode.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import QRCode from "react-qr-code";
-import "./GenerateQRCode.css";
-import { jwtDecode } from 'jwt-decode';
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+import QRCode from "react-qr-code"
+import "./GenerateQRCode.css"
+import { jwtDecode } from 'jwt-decode'
 
 
 const GenerateQRCode = () => {
-    const [userId, setUserId] = useState("");
+    const [userId, setUserId] = useState("")
 
     useEffect(() => {
-	const token = localStorage.getItem('token');
-	console.log(token);
+	const token = localStorage.getItem('token')
 	if (!token) {
 	console.error('No token found');
 	return;
@@ -18,21 +17,21 @@ const GenerateQRCode = () => {
       
 	const fetchUserId = async () => {
 	try {
-	const decodedToken = jwtDecode(token);
+	const decodedToken = jwtDecode(token)
 	const response = await axios.post(`${process.env.REACT_APP_SERVER_HOSTNAME}/GenerateQRCode`, { decodedToken }, {
 		headers: {
 		Authorization: `Bearer ${token}`
 		}
 	});
       
-	setUserId(response.data.decodedToken.userId);
-	console.log(response.data.decodedToken.userId);
+	setUserId(response.data.decodedToken.userId)
+	console.log(response.data.decodedToken.userId)
 	} catch (error) {
-	console.error("Error fetching user ID:", error);
+	console.error("Error fetching user ID:", error)
 	}
 	};
       
-	fetchUserId();
+	fetchUserId()
       }, []);
       
 
