c130fd1daf7189fd834dd1c9e151b15a5e4345eb
SivanC <sc7443@nyu.edu>
2023-12-05 02:00:32 -0500

    Fix connection tests

--- back-end/connections.js
+++ back-end/connections.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const { User } = require('./models/User')
-const authenticateToken = require('./authRoutes')
 const { param, validationResult } = require('express-validator')
 const router = express.Router()
 const defaultImage = '/default.png' 
 
-const validateObjectId = (paramName) => {
+const validateObjectId = (paramName) => [
   param(paramName).isMongoId(),
   (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      res.status(400).json({ errors: errors.array() })
     }
     next()
   }
-}
+]
 
 const handleValidationErrors = (req, res, next) => {
   const errors = validationResult(req)
@@ -25,10 +24,10 @@ const handleValidationErrors = (req, res, next) => {
   next()
 }
 
-router.get('/:id', authenticateToken, validateObjectId('id'), async (req, res) => {
+router.get('/:id', validateObjectId('id'), async (req, res) => {
   try {
-    const user = await User.findById(req.params.userId).exec()
-    if (!user || !user.connections) {
+    const user = await User.findById(req.params.id).exec()
+    if (!user || !user.connections || res.stats === 400) {
       return res.status(404).json({ error: 'User not found or no connections available' })
     }
     res.json(user.connections)
@@ -38,15 +37,19 @@ router.get('/:id', authenticateToken, validateObjectId('id'), async (req, res) =
   }
 })
 
-router.delete('/:userId/:friendId', authenticateToken, validateObjectId('userId'), validateObjectId('friendId'), async (req, res) => {
+router.delete('/:userId/:friendId', 
+  validateObjectId('userId'), 
+  validateObjectId('friendId'), 
+  async (req, res) => {
   try {
     const user = await User.findByIdAndUpdate(
       req.params.userId,
       { $pull: { connections: { friend_id: req.params.friendId } } },
       { new: true }
     )
+    console.log(res)
 
-    if (!user) {
+    if (!user || res.status === 400) {
       return res.status(404).send('User not found')
     }
 
